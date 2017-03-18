d3cf25f5b3871fd6548db9fa6ea9afd023d4fc21
Alvar Lumberg <alvar@transferwise.com>
2017-03-19 00:21:12 +0200

    makes upload.html nicer and submits file for parsing/storing

--- src/main/java/eu/cryptoeuro/bankgateway/rest/StatementController.java
+++ src/main/java/eu/cryptoeuro/bankgateway/rest/StatementController.java
@@ -1,6 +1,9 @@
 package eu.cryptoeuro.bankgateway.rest;
 
+import eu.cryptoeuro.bankgateway.services.transaction.TransactionService;
+import eu.cryptoeuro.bankgateway.services.transaction.model.Transaction;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,9 +11,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.File;
+
 @Controller
 @Slf4j
 public class StatementController {
+	@Autowired
+	TransactionService transactionService;
+
 	@GetMapping("/statement")
 	public String uploadForm() {
 		return "upload";
@@ -19,8 +27,16 @@ public class StatementController {
 	@PostMapping("/statement")
 	public String handleFileUpload(@RequestParam("statementFile") MultipartFile file,
 																 RedirectAttributes redirectAttributes) {
-		redirectAttributes.addFlashAttribute("message",
-						"You successfully uploaded '" + file.getOriginalFilename() + "' ("+ file.getSize() +" bytes)!");
+		try {
+			File transactionFile = File.createTempFile(file.getOriginalFilename(), ".tmp");
+			file.transferTo(transactionFile);
+			transactionService.importTransactions(transactionFile, Transaction.Source.FILE_IMPORT);
+			redirectAttributes.addFlashAttribute("message",
+							"You successfully uploaded '" + file.getOriginalFilename() + "' ("+ file.getSize() +" bytes)!");
+		} catch (Exception e) {
+			log.error("error processing uploaded statement file", e);
+			redirectAttributes.addFlashAttribute("error", "error processing uploaded file: " + e.getMessage());
+		}
 
 		return "redirect:/statement";
 	}
